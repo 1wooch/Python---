8fb4286fdffa30f3ed43fab678c38b1090a1c1e1
1wooch <onechoo1106@gmail.com>
2021-12-20 23:05:47 +0900

    crowling extrememovie

---
+++ file/크롤링/crowling.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# Anchor extraction from HTML document
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+title=[]
+f=open("x.txt",'w')
+
+
+response=urlopen('https://extmovie.com/movietalk') 
+soup = BeautifulSoup(response, 'html.parser')
+#for anchor in soup.select("a.title_link"):
+#for anchor in soup.find_all('div',data-pswp-uid_=True):
+#for anchor in soup.find_all('div',data-pswp-uid_ = true):
+#    print(anchor)
+for anchor in soup.find_all("div",{"class":"title_area"}):#{"class":"title_link"}):
+#find_all("div",{"class":"title_area"}):#.find_all("a",{"class":"title_link"}):
+#p=soup.find_all("div",{"class":"title_area"}).find_all("a",{"class":"title_link"})
+    #print(anchor.get_text())
+    title.append(anchor.get_text())
+#print(title)
+#del title[0:len(title):2]
+for i in title:
+    print(i[8:-5])
+    #data=i[7:-5]
+    i
+    f.write(str(i[8:-5])+"\n")
+    #i.strip("\t")
+
