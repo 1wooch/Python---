e0d5cb61651a82f54e8da5dd4e40ddba3bb383e3
1wooch <onechoo1106@gmail.com>
2021-12-27 23:07:46 +0900

    2021-12-27

--- file/크롤링/셀레니움_테스트.py/test1.py
+++ file/크롤링/셀레니움_테스트.py/test1.py
@@ -4,14 +4,14 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time 
 #from find_text import check_string
-f=open("x.txt",'w',encoding='UTF-8')
+f=open("소진지점.txt",'w',encoding='UTF-8')
 
 s=Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=s)
 driver.maximize_window()
 driver.get('https://extmovie.com/movietalk/71696227')
 driver.implicitly_wait(5)
-
+소진지점=[]
 
 
 while True:
@@ -23,6 +23,7 @@ while True:
         time.sleep(1)
         #더보기.click()
         #time.sleep(1)
+
     except:
         break
 
@@ -32,14 +33,39 @@ for content in contents:
     #if '소진' in content:
     #    print(content)
     f.write(content.text+"\n")
-print("1")
+
+f.close()
+
+
 def check_string():
-    with open('x.txt', 'rt', encoding='UTF8') as temp_f:
+    with open('소진지점.txt', 'rt', encoding='UTF8') as temp_f:
         datafile = temp_f.readlines()
     for line in datafile:
         if '소진' in line:
             print(line)
+            소진지점.append(line)
         elif '끝' in line:
                 print(line)
-print("2")
+                소진지점.append(line)
+
+
+
 check_string()
+print(소진지점)
+f = open('소진지점.txt', 'r+', encoding='UTF8')
+f.truncate(0) # 파일비우기
+data=f.readlines()
+for a in data:
+    print(a) #비워졌는지 체크
+
+for element in 소진지점:
+    f.write(element) #텍스트파일에 적기
+
+for line in data:
+    print(line) #채워졌는지 체크
+
+f.close()
+print("end")
+
+
+driver.close()
