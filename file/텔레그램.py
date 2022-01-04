7b224e213b75ea96990acb5a80c3ac7ec24782bb
1wooch <onechoo1106@gmail.com>
2022-01-04 23:16:11 +0900

    2022-01-04

--- file/텔레그램.py
+++ file/텔레그램.py
@@ -2,11 +2,5 @@ import telegram
 토큰='5081033456:AAFC9HDCGCdGyvKe2inpuxkHfKSV7Ab1Rww'
 봇 = telegram.Bot(token=토큰)
 
-#for i in 봇.getUpdates():
-#    print(i.message)
 
-
-#chat id =1054405622
-#https://api.telegram.org/bot5081033456:AAFC9HDCGCdGyvKe2inpuxkHfKSV7Ab1Rww/getUpdates
-# 봇.send_message("안녕")
 봇.send_message(1054405622,"hi")
