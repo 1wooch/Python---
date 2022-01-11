f2d1742bf48ecb077b1101021315a8b1795bba1e
1wooch <onechoo1106@gmail.com>
2022-01-12 06:30:30 +0900

    2022-01-12

--- file/크롤링/셀레니움_테스트.py/test2.py
+++ file/크롤링/셀레니움_테스트.py/test2.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.service import Service
+from urllib3.packages.six import StringIO
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time 
 import csv
 import telegram
+import re
 #from find_text import check_string
 f=open("소진지점.txt",'w',encoding='UTF-8')
 
@@ -20,14 +22,24 @@ driver.implicitly_wait(5)
 소진지점=[] #before sorting
 소진지점2=[] #after sorting by '소진' ,'끝'
 테스트=""
-CGV서울=['강변','대학로','등촌','불광','상봉','성신여대입구','수유','신촌','압구정','여의도','연남','영등포','왕십리','용산','중계','천호']
-CGV경기=['광교','구리','김포','동탄','배곧','소풍','수원','안산','의정부','의정부태흥','이천','일산','죽전','파주야당','판교','평촌','평택']
-CGV인천=['인천','계양','부평','인천','인천연수','청라']
-CGV충남=['대전','대전탄방','대전터미널','세종','천안터미널','천안펜타포트','청주서문']
-CGV대구=['대구스타디움','대구아카데미','대구한일','대구현대']
-CGV전남=['광주금남로','광주첨단','광주터미널','목포평화광장','순천신대','여수웅천','전주고사','전주효자']
-CGV강원=['원주','춘천']
-CGV부산=['경산','김해','동래','북포항','서면','서면삼정타워','센텀시티','양산물금','울산삼산','창원더시티','하단아트몰링']
+# CGV서울=['강변','대학로','등촌','불광','상봉','성신여대입구','수유','신촌','압구정','여의도','연남','영등포','왕십리','용산','중계','천호']
+# CGV경기=['광교','구리','김포','동탄','배곧','소풍','수원','안산','의정부','의정부태흥','이천','일산','죽전','파주야당','판교','평촌','평택']
+# CGV인천=['인천','계양','부평','인천','인천연수','청라']
+# CGV충남=['대전','대전탄방','대전터미널','세종','천안터미널','천안펜타포트','청주서문']
+# CGV대구=['대구스타디움','대구아카데미','대구한일','대구현대']
+# CGV전남=['광주금남로','광주첨단','광주터미널','목포평화광장','순천신대','여수웅천','전주고사','전주효자']
+# CGV강원=['원주','춘천']
+# CGV부산=['경산','김해','동래','북포항','서면','서면삼정타워','센텀시티','양산물금','울산삼산','창원더시티','하단아트몰링']
+
+CGV서울=[]
+CGV경기=[]
+CGV인천=[]
+CGV충남=[]
+CGV대구=[]
+CGV전남=[]
+CGV강원=[]
+CGV부산=[]
+
 
 CGV서울소진필름마크=[]
 CGV서울소진아맥포스터=[]
@@ -55,25 +67,42 @@ CGV부산소진아맥포스터=[]
 
 CGV=[]
 MegaBox=[]
+MB오리지널티켓서울=[]
 LotteCinema=[]
+LC아트카드서울=[]
+
+with open('cgv.txt','r',encoding='UTF-8') as fd:
+    reader=csv.reader(fd)
+    for row in reader:
+        CGV.append(row)
+CGV서울.extend(CGV[0])
+CGV경기.extend(CGV[1])
+CGV인천.extend(CGV[2])
+CGV충남.extend(CGV[3])
+CGV대구.extend(CGV[4])
+CGV전남.extend(CGV[5])
+CGV강원.extend(CGV[6])
+CGV부산.extend(CGV[7])
+with open('megabox.txt','r',encoding='UTF-8') as fd:
+    reader=csv.reader(fd)
+    for row in reader:
+        MegaBox.append(row)
+MB오리지널티켓서울.extend(MegaBox[0])
+#megabox
 
 
-# with open('cgv.txt','r',encoding='UTF-8') as fd: #cgv 지점 목록 파일에서 
-#     reader=csv.reader(fd)
-#     for row in reader:
-#         CGV.append(row)
-# CGV서울.extend(CGV[0])
-# CGV경기.extend(CGV[1])
-# CGV인천.extend(CGV[2])
-# CGV충남.extend(CGV[3])
-# CGV대구.extend(CGV[4])
-# CGV전남.extend(CGV[5])
-# CGV강원.extend(CGV[6])
-# CGV부산.extend(CGV[7])
 
+with open('Lotte_cinema.txt','r',encoding='UTF-8') as fd:
+    reader=csv.reader(fd)
+    for row in reader:
+        LotteCinema.append(row)
 
+LC아트카드서울.extend(LotteCinema[0])
 
-#cgv 지점 등록
+print(LC아트카드서울)
+
+print(LC아트카드서울[0])
+
 
 while True:
     try:
@@ -94,51 +123,25 @@ contents = driver.find_elements_by_css_selector('div.cmt_body')
 for cont in contents:
     소진지점.append(cont.text)
 
-for content in 소진지점:
-    f.write(content+"\n")
-f.close()
 
 def check_string():
-    with open('소진지점.txt', 'rt', encoding='UTF8') as temp_f:
-        datafile = temp_f.readlines()
-    for line in datafile:
+    
+    for line in 소진지점:
         
         if '소진' in line:
-           # print(line)
+            print(line)
             소진지점2.append(line)
         elif '끝' in line:
-               # print(line)
+                print(line)
                 소진지점2.append(line)
 
 check_string()
 
 
-#print(소진지점2)
-f = open('소진지점.txt', 'r+', encoding='UTF8')
-f.truncate(0) # 파일비우기
-data=f.readlines()
-#for a in data:
-    #print(a) #비워졌는지 체크
 
-for element in 소진지점2:
-    f.write(element) #텍스트파일에 적기
-
-#for line in data:
-    #print(line) #채워졌는지 체크
-
-f.close()
 
 
 driver.close()
-
-
-# def 지역별소진(region,item):
-#     region.case_name=
-        
-        
-
-
-
 
 
 def 서울소진( ): #in () -> user can put 필름마크 or 아맥 포스터
@@ -152,8 +155,7 @@ def 서울소진( ): #in () -> user can put 필름마크 or 아맥 포스터
                 
                 elif '아맥포스터' in s2:
                     CGV서울소진아맥포스터.append(s1)
-    # print('서울 필름마크 소진지점: ',CGV서울소진필름마크)
-    # print('서울 아이맥스 포스터 소진지점: ',CGV서울소진아맥포스터)
+    
 
         
 
@@ -169,8 +171,7 @@ for s1 in CGV경기:
             elif '아맥포스터' in s2:
                 CGV경기소진아맥포스터.append(s1)
 
-# print('경기 필름마크 소진지점: ',CGV경기소진필름마크)
-# print('경기 아이맥스 포스터 소진지점: ',CGV경기소진아맥포스터)
+
 
 
 for s1 in CGV인천:
@@ -183,8 +184,7 @@ for s1 in CGV인천:
                 CGV인천소진아맥포스터.append(s1)
                
 
-# print('인천 필름마크 소진지점: ',CGV인천소진필름마크)
-# print('인천 아이맥스 포스터 소진지점: ',CGV인천소진아맥포스터)
+
 
 for s1 in CGV충남:
     for s2 in 소진지점2:
@@ -196,8 +196,7 @@ for s1 in CGV충남:
                 CGV충남소진아맥포스터.append(s1) 
 
 
-# print('충남 필름마크 소진지점: ',CGV충남소진필름마크)
-# print('충남 아이맥스 포스터 소진지점: ',CGV충남소진아맥포스터)
+
 
 
 for s1 in CGV대구:
@@ -211,8 +210,6 @@ for s1 in CGV대구:
                 
 
 
-# print('대구 필름마크 소진지점: ',CGV대구소진필름마크)
-# print('대구 아이맥스 포스터 소진지점: ',CGV대구소진아맥포스터)
 
 for s1 in CGV전남:
     for s2 in 소진지점2:
@@ -225,8 +222,7 @@ for s1 in CGV전남:
                 
 
 
-# print('전남 필름마크 소진지점: ',CGV전남소진필름마크)
-# print('전남 아이맥스 포스터 소진지점: ',CGV전남소진아맥포스터)
+
 
 for s1 in CGV강원:
     for s2 in 소진지점2:
@@ -238,8 +234,7 @@ for s1 in CGV강원:
                 CGV강원소진아맥포스터.append(s1)
                
 
-# print('강원 필름마크 소진지점: ',CGV강원소진필름마크)
-# print('강원 아이맥스 포스터 소진지점: ',CGV강원소진아맥포스터)
+
 
 for s1 in CGV부산:
     for s2 in 소진지점2:
@@ -250,14 +245,42 @@ for s1 in CGV부산:
             elif '아맥포스터' in s2:
                 CGV부산소진아맥포스터.append(s1)
 
+new_list=[]
+def 정렬(listname):
+    global result1
+    global listn
+    result1=set(listname)
 
-# print('부산 필름마크 소진지점: ',CGV부산소진필름마크)
-# print('부산 아이맥스 포스터 소진지점: ',CGV부산소진아맥포스터)
+    fixedlist=list(result1)
+    listname.clear()
+    for v in fixedlist:
+        listname.append(v)
+    #중복자료제거
+    listname.sort()
+   
+정렬(CGV서울소진필름마크)
+정렬(CGV서울소진아맥포스터)
+정렬(CGV부산소진필름마크)
+정렬(CGV부산소진아맥포스터)
+정렬(CGV대구소진필름마크)
+정렬(CGV대구소진아맥포스터)
+정렬(CGV경기소진필름마크)
+정렬(CGV경기소진아맥포스터)
+정렬(CGV강원소진필름마크)
+정렬(CGV강원소진아맥포스터)
+정렬(CGV충남소진아맥포스터)
+정렬(CGV충남소진필름마크)
+정렬(CGV전남소진아맥포스터)
+정렬(CGV전남소진필름마크)
+정렬(CGV인천소진아맥포스터)
+정렬(CGV서울소진필름마크)
+
+
 
 class 굿즈소진:
     class 지역필름마크소진:
         def __init__(self):
-            self.서울='서울 필름마크 소진지점: ',''.join(CGV서울소진필름마크)
+            self.서울='서울 필름마크 소진지점: ',CGV서울소진필름마크
             self.부산='부산 필름마크 소진지점: ',str(CGV부산소진필름마크)
             self.경기='경기 필름마크 소진지점: ',CGV경기소진필름마크
             self.인천='인천 필름마크 소진지점: ',CGV인천소진필름마크
